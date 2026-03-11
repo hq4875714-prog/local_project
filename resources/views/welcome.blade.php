cf816dcd77cdf79d53299101fd40a2c24dcacc4f
Hassan Qureshi <hq4875714@gmail.com>
2026-03-11 15:01:29 +0500

    Add user greeting section to welcome page
    Added a section with a title and placeholder text.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,6 +30,17 @@
                         >
                             Dashboard
                         </a>
+
+                     <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                <h2>  Ali khan </h2>
+                    <p>loren bfhfe c  ce ce ebbe eubferbfe</p>
+                </div>
+            </div>
+        </div>
+    </div>
                     @else
                         <a
                             href="{{ route('login') }}"
